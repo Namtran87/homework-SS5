a5c4dea2bfa76615cb2ddc5726df08c8a9cd1394
Namtran87 <67421678+Namtran87@users.noreply.github.com>
2020-09-07 16:27:38 +0700

    hw

--- register.js
+++ register.js
@@ -3,11 +3,21 @@ function addData() {
     let lname = document.getElementById("lname").value;
     let email = document.getElementById("email").value;
     let pnumber = document.getElementById("pnumber").value;
+    console.log(fname)
     let data = {
         firstname: fname,
         lastname: lname,
         email: email,
         phoneNumber: pnumber
     }
-    firebase.firestore().collection('users').add(data);
+    firebase.firestore().collection('users').add(data).value;
+    document.getElementById("fgroup").reset();
+}
+async function readData() {
+    let result = await firebase.firestore().collection('users').get();
+    for (let doc of result.docs) {
+        console.log(doc.data());
+        doc.data()
+
+    }
 }
